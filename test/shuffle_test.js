0af10673d15204c8e460004c22b67176fdbbd49e
saivenkat444 <kamanasaivenkat444@gmail.com>
2025-05-14 17:48:18 +0530

    added a passing and failing test case

--- test/shuffle_test.js
+++ test/shuffle_test.js
@@ -1,8 +1,15 @@
-import { assertEquals } from "jsr:@std/assert";
-
+import { assert, assertEquals } from "jsr:@std/assert";
+import { add } from "../main.js";
 
 //sample test function for reference 
-Deno.test("simple test", () => {
-  const x = 1 + 2;
-  assertEquals(x, 3);
+Deno.test("PASSING test", () => {
+  const actual = add(2, 3);
+  const expected = 5;
+  assertEquals(actual, expected)
+});
+
+Deno.test("FAILING test", () => {
+  const actual = add(2, 3);
+  const expected = 7;
+  assertEquals(actual, expected)
 });
